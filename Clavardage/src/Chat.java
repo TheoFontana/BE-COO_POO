cfc59ee2f38b26248348d5cab31bc211ecc7034a
David <David@HP-TLS>
2020-12-09 15:14:55 +0100

    mise a jour fichier chat

--- Clavardage/src/Chat.java
+++ Clavardage/src/Chat.java
@@ -1,60 +1,116 @@
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.io.*;
+import java.net.*;
+import java. util.*;
 
 
-public class Chat {
+
+
+public class Chat implements Runnable {
 	
-	public static void main(String[] args) {
-
-	        try {
-	            ServerSocket ss = new ServerSocket(1234);
-
-	            while(true) {
-	                System.out.println("Awaiting connection");
-	                Socket link = ss.accept();
-	                new Thread(new Runner(link)).start();
-	            }
-
-	        } catch (Exception e) {
-	            e.printStackTrace();
-	        }
-
-
-	    }
-
-
-
+	final ServerSocket serveur;
+	final Socket client;
+	final BufferedReader in;
+	final PrintWriter out;
+	final Scanner scan = new Scanner(System.in);
+	
+	try {
+		
+		serveur = new ServerSocket(5000);
+		client = serveur.accept();
+		out = new PrintWriter(client.getOutputStream());
+		in = new BufferedReader(new InputStreamReader (client.getInputStream());
+		Thread envoi = new Thread();
+		envoi.start();
+		String msg;
+		
+		public void run() {
+			while(true) {
+				msg = scan.nextLine();
+				out.println(msg);
+				out.flush();
+			}
+		}
+		
+		
+		Thread recois = new Thread();
+		recois.start();
+		String msg;
+		
+		public void run() {
+			try {
+				msg = in.readLine();
+				
+				while(msg!=null) {
+					System.out.println("Client:" msg);
+					msg = in .readLine();
+				}
+				
+				out.close();
+				client.close();
+				serveur.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
 
 
-class Runner implements Runnable {
+-----------------------------------------------------------
 
-	    final Socket link;
 
-	    public Runner(Socket link) {
-	        this.link = link;
-	    }
+import java.io.*;
+import java.net.*;
+import java.util.*;
 
-	    @Override
-	    public void run() {
-	        System.out.println("Thread started");
-	        try {
-	            InputStream is = link.getInputStream();
-	            int rcv = 0;
-	            while ((rcv = is.read()) != 0) {
-	                System.out.println("Received: "+rcv);
-	            }
-	            System.out.println("Finishing thread");
-	            is.close();
-	            link.close();
-	        } catch (Exception e) {
-	            e.printStackTrace();
-	        }
-	    }
-}
+
+public class ChatClient implements Runnable {
 	
-
-
-
+	final Socket client;
+	final BufferedReader in;
+	final PrintWriter out;
+	final scan = new Scanner(System.in);
+	
+	try {
+		// Adresse local en attendant savoir comment recuperer adresse du serveur
+		client = new Socket("127.0.0.1", 5000);
+		
+		out = new PrintWriter(client.getOutputStream());
+		
+		in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+		
+		Thread envoyer = new Thread();
+		envoyer.start();
+		
+		String msgenv;
+		
+		public void run() {
+			while(true) {
+				msgenv = sc.nextLine();
+				out.println(msgenv);
+				out.flush();
+			}
+		}
+		
+		
+		Thread recevoir = new Thread();
+		
+		String msgrc;
+		
+		public void run() {
+			try {
+				msgrc = in.readLine();
+				while(msgrc != null) {
+					System.out.println("Serveur:" + msgrc);
+					msgrc = in.readLine();
+				}
+				out.close();
+				client.close();
+				
+				} catch (IOEXception e) {
+					e.printStackTrace();
+				}
+		}
+		
+	}
+}
